e1b56372926bac2c9596baa2ccc59facbfb32a94
SamirIngley <samir.ingle7@gmail.com>
2019-10-10 18:39:46 -0700

    Procfile

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
+
 client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
 
 app = Flask(__name__)
-#
+
 
 
 # OUR MOCK ARRAY OF PROJECTS
